e95c27e06dfc157066627e92517b123fd82d4690
BulRod <saul_gr@tesch.edu.mx>
2024-02-28 15:28:36 -0600

    Perfiles completos versión final

--- app/src/main/java/com/secaac/mimec/perfil/PerfilM.kt
+++ app/src/main/java/com/secaac/mimec/perfil/PerfilM.kt
@@ -1,86 +1,236 @@
-package com.secaac.mimec
+package com.secaac.mimec.perfil
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.secaac.mimec.MainActivity
+import com.secaac.mimec.R
+import com.secaac.mimec.Rcontrasena_M
+import java.util.UUID
 
 class PerfilM : Fragment() {
 
     private val db = Firebase.firestore
+    private val PICK_IMAGE_REQUEST = 71
+    private var filePath: Uri? = null
+    private lateinit var imageView5: ImageView
+    private lateinit var etNombreCompleto: TextView
+    private lateinit var etNombreTaller: TextView
+    private lateinit var etCorreo: TextView
+    private lateinit var etDireccion: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflar el layout para este fragmento
         val vista = inflater.inflate(R.layout.fragment_perfil_m, container, false)
+        val user = FirebaseAuth.getInstance().currentUser
+        imageView5 = vista.findViewById(R.id.imageView5)
+        etNombreCompleto = vista.findViewById(R.id.nombre_m)
+        etNombreTaller = vista.findViewById(R.id.n_taller)
+        etCorreo = vista.findViewById(R.id.correo_m)
+        etDireccion = vista.findViewById(R.id.direccioon)
 
-        // Obtener los datos del usuario de Firestore
-        obtenerDatosUsuario(vista)
+        Glide.with(this)
+            .load(user?.photoUrl)
+            .apply(RequestOptions.circleCropTransform())
+            .into(imageView5)
 
-        // Agregar OnClickListener al botón "Cerrar sesión"
+        imageView5.setOnClickListener {
+            launchGallery()
+        }
+
         vista.findViewById<Button>(R.id.cerrarSM).setOnClickListener {
             cerrarSesion()
         }
 
-        // Agregar OnClickListener al TextView "Cambiar contraseña"
         vista.findViewById<TextView>(R.id.btn_cambiarc).setOnClickListener {
-            // Lógica para iniciar la nueva actividad cuando se hace clic en el TextView
             iniciarNuevaActividad()
         }
+
+        cargarDatosMecanico()
 
         return vista
     }
 
-    private fun obtenerDatosUsuario(vista: View) {
-        // Obtener el correo electrónico del usuario actual
-        // Reemplaza "correoUsuarioActual" con el correo electrónico real del usuario actual
-        val correoUsuarioActual = "correoUsuarioActual"
-
-        db.collection("usuarios")
-            .whereEqualTo("correo", correoUsuarioActual)
+    override fun onResume() {
+    super.onResume()
+    // Obtener la URL de la imagen desde Firestore
+    val db = Firebase.firestore
+    val userId = FirebaseAuth.getInstance().currentUser?.uid
+    if (userId != null) {
+        db.collection("usuarios").document(userId)
             .get()
-            .addOnSuccessListener { documentos ->
-                for (documento in documentos) {
-                    // Encontrar los TextViews
-                    val nombreMTextView = vista.findViewById<TextView>(R.id.nombre_m)
-                    val correoMTextView = vista.findViewById<TextView>(R.id.correo_m)
-                    val nombreTallerTextView = vista.findViewById<TextView>(R.id.n_taller)
-                    val direccionTextView = vista.findViewById<TextView>(R.id.direccion)
-
-                    // Establecer el texto de los TextViews con los datos del usuario
-                    nombreMTextView.text = documento.getString("nombreCompleto")
-                    correoMTextView.text = documento.getString("correo")
-                    nombreTallerTextView.text = documento.getString("nombreTaller")
-                    direccionTextView.text = documento.getString("direccion")
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    val photoUrl = document.getString("photoUrl")
+                    // Utilizar Glide para cargar la imagen
+                    Glide.with(this)
+                        .load(photoUrl)
+                        .apply(RequestOptions.circleCropTransform())
+                        .into(imageView5)
                 }
             }
-            .addOnFailureListener { excepcion ->
-                Log.d("PerfilM", "falló la obtención con ", excepcion)
-            }
+    }
+}
+
+    private fun launchGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, PICK_IMAGE_REQUEST)
     }
 
-    // Función para cerrar la sesión
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK
+            && data != null && data.data != null )
+        {
+            val tempUri = data.data
+            filePath = copyImageToAppStorage(tempUri)
+            try {
+                // Utilizar Glide para cargar la imagen
+                Glide.with(this)
+                    .load(filePath)
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(imageView5)
+                uploadImage()
+            }
+            catch (e: Exception){
+                e.printStackTrace()
+                // Aquí puedes manejar el error, por ejemplo, mostrando un mensaje al usuario
+            }
+        }
+    }
+    private fun copyImageToAppStorage(uri: Uri?): Uri? {
+        val inputStream = activity?.contentResolver?.openInputStream(uri!!)
+        val outputStream = activity?.openFileOutput("profile_picture.jpg", Context.MODE_PRIVATE)
+        if (outputStream != null) {
+            inputStream?.copyTo(outputStream)
+        }
+        inputStream?.close()
+        outputStream?.close()
+        return Uri.fromFile(activity?.getFileStreamPath("profile_picture.jpg"))
+    }
+
+  private fun cargarDatosMecanico() {
+    val db = Firebase.firestore
+
+    // Obtener el ID del usuario actualmente autenticado
+    val userId = FirebaseAuth.getInstance().currentUser?.uid
+
+    if (userId != null) {
+        Log.d("PerfilM", "User ID: $userId") // Registrar el ID del usuario
+
+        db.collection("usuarios").document(userId)
+            .addSnapshotListener { snapshot, e ->
+                if (e != null) {
+                    Log.w("PerfilM", "Listen failed.", e)
+                    return@addSnapshotListener
+                }
+
+                if (snapshot != null && snapshot.exists()) {
+                    Log.d("PerfilM", "Current data: ${snapshot.data}") // Registrar los datos del documento
+
+                    val nombreCompleto = snapshot.getString("nombreCompleto")
+                    val nombreTaller = snapshot.getString("nombreTaller")
+                    val correo = snapshot.getString("correo")
+                    val direccion = snapshot.getString("direccion")
+                    val photoUrl = snapshot.getString("photoUrl")
+
+                    if (nombreCompleto == null || nombreTaller == null || correo == null || direccion == null || photoUrl == null) {
+                        Log.d("PerfilM", "One or more fields are null")
+                    }
+
+                    activity?.runOnUiThread {
+                        etNombreCompleto.text = nombreCompleto
+                        etNombreTaller.text = nombreTaller
+                        etCorreo.text = correo
+                        etDireccion.text = direccion
+
+                        // Cargar la imagen desde la URL
+                        Glide.with(this)
+                            .load(photoUrl)
+                            .apply(RequestOptions.circleCropTransform())
+                            .into(imageView5)
+                    }
+                } else {
+                    Log.d("PerfilM", "Current data: null")
+                }
+            }
+    } else {
+        Log.d("PerfilM", "User ID is null")
+    }
+}
+
+    private fun uploadImage() {
+        if(filePath != null) {
+            val db = Firebase.firestore
+            val userId = FirebaseAuth.getInstance().currentUser?.uid
+            if (userId != null) {
+                db.collection("usuarios").document(userId)
+                    .get()
+                    .addOnSuccessListener { document ->
+                        if (document != null) {
+                            val oldImageUrl = document.getString("photoUrl")
+                            if (oldImageUrl != null) {
+                                val oldImageRef = FirebaseStorage.getInstance().getReferenceFromUrl(oldImageUrl)
+                                oldImageRef.delete()
+                            }
+                        }
+                    }
+            }
+
+            val ref = FirebaseStorage.getInstance().getReference("Fotos perfil usuario/" + UUID.randomUUID().toString())
+            ref.putFile(filePath!!)
+                .addOnSuccessListener {
+                    ref.downloadUrl.addOnSuccessListener { uri ->
+                        val user = FirebaseAuth.getInstance().currentUser
+                        val profileUpdates = UserProfileChangeRequest.Builder()
+                            .setPhotoUri(uri)
+                            .build()
+                        user?.updateProfile(profileUpdates)
+
+                        if (userId != null) {
+                            db.collection("usuarios").document(userId)
+                                .update("photoUrl", uri.toString())
+                        }
+
+                        Glide.with(this)
+                            .load(uri)
+                            .apply(RequestOptions.circleCropTransform())
+                            .into(imageView5)
+                    }
+                }
+                .addOnFailureListener { e ->
+                }
+        }
+    }
+
     private fun cerrarSesion() {
-        // Crear un Intent para iniciar MainActivity
+        FirebaseAuth.getInstance().signOut() // Cerrar la sesión del usuario actual
         val intent = Intent(activity, MainActivity::class.java)
-        // Iniciar MainActivity
         startActivity(intent)
     }
 
-    // Función para iniciar la nueva actividad
     private fun iniciarNuevaActividad() {
-        // Crear un Intent para iniciar la nueva actividad (reemplaza NuevaActividad::class.java con el nombre real de tu nueva actividad)
         val intent = Intent(activity, Rcontrasena_M::class.java)
-        // Iniciar la nueva actividad
         startActivity(intent)
     }
 }
--- app/src/main/java/com/secaac/mimec/register/RegisterUActivity.kt
+++ app/src/main/java/com/secaac/mimec/register/RegisterUActivity.kt
@@ -1,4 +1,4 @@
-package com.secaac.mimec
+package com.secaac.mimec.register
 
 import android.content.Intent
 import android.os.Bundle
@@ -9,6 +9,8 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import com.secaac.mimec.LoginUActivity
+import com.secaac.mimec.R
 
 class RegisterUActivity : AppCompatActivity() {
     private lateinit var txtIni: TextView
